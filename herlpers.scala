6f31694539744f8553d6aea5f0e122d5b2761b4e
SoerenSofke <16596310+SoerenSofke@users.noreply.github.com>
2025-04-06 14:32:52 +0000

    use TestSuite instead object

--- herlpers.scala
+++ herlpers.scala
@@ -1,21 +1,16 @@
-// Testing the functions utilizing the "uTest" framework
 package helpers
 
-// Implementing functions
+// Implementing helper functions
 def factorial(n: Int): Int =
   if n <= 1 then 1
   else n * factorial(n - 1)
 
-
+// Testing the helper functions utilizing the "uTest" framework
 import utest._
 
-private val helperTests = Tests:
-  test("factorial"):
-    val f = factorial(6)
-    f ==> 720
-
-// Running test suite
-@main
-private def main(): Unit =
-  val results = TestRunner.runAndPrint(helperTests, "helpers")
+private object CalculatorTest extends TestSuite:
+   val tests = Tests:
+    test("factorial"):
+      val f = factorial(6);  
+      f ==> 720
 
